ee6fb17acc06d1daeb4435fe2ec454c028896f84
Ronson Lui <rlui001@ucr.edu>
2017-02-11 03:58:44 -0800

    Completed node expansion method

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ final_state = [[1,2,3],
 def misplaced(problem):
 	""" 
 	Counts total misplaced tiles in the puzzle, 
-	returns as the heuristic value
+	returns as the heuristic value.
 	"""
 	heuristic = 0
 	for i in range(3):
@@ -20,6 +20,101 @@ def misplaced(problem):
 
 	return heuristic
 
+def manhattan(problem):
+	"""
+	Returns the total manhattan distance in the puzzle.
+	Manhattan distance formula = abs(x1 - x0) + abs(y1 - y0)
+	"""
+	heuristic = 0
+
+	# The range of the first for loop == all the numbers in the puzzle
+	# i.e. in the 8-puzzle we have 1-8 --> for x in range(1,9)
+	# if puzzle is changed, change the range accordingly
+	for x in range(1,9):
+		for i in range(3):
+			for j in range(3):
+				# We obtain the row and col of the curr state and final state
+				# then calculate the distance between them, and add it to heuristic
+				if x == problem[i][j]:
+					problemRow = i
+					problemCol = j
+				if x == final_state[i][j]:
+					finalRow = i 
+					finalCol = j
+		heuristic += (abs(problemRow - finalRow) + abs(problemCol - finalCol))
+	return heuristic
+
+def expand(currNode, nodes_expanded):
+	"""
+	This method expands the current node by getting performing the operations
+	(left, right, up, down) on the puzzle and also updates the total
+	number of nodes expanded. Both results are returned: list, nodes_expanded
+	"""
+
+	# Empty list to store the new nodes
+	# depth to update
+	List = []
+	depth = currNode.depth + 1
+	# find location of '0' in the puzzle
+	blankRow = 0
+	blankCol = 0
+	for i in range(3):
+		for j in range(3):
+			if currNode.puzzle_state[i][j] == 0:
+				blankRow = i 
+				blankCol = j
+
+
+	# top copies the puzzle, and then determines/swaps with top tile if possible
+	# check if blank can swap with top
+	if blankRow != 0:
+		top = currNode.puzzle_state
+		# replace the 0 with the value above it
+		top[blankRow][blankCol] = top[blankRow - 1][blankCol]
+		# value above it now holds the value 0
+		top[blankRow - 1][blankCol] = 0
+		# create node object, append to the list
+		topNode = Node(0, depth, top)
+		List.append(topNode)
+		nodes_expanded += 1
+
+	# perform check/swap for the other operations
+	if blankRow != 2:
+		bot = currNode.puzzle_state
+
+		bot[blankRow][blankCol] = bot[blankRow + 1][blankCol]
+
+		bot[blankRow + 1][blankCol] = 0
+
+		botNode = Node(0, depth, bot)
+		List.append(botNode)
+		nodes_expanded += 1
+
+	if blankCol != 0:
+		left = currNode.puzzle_state
+
+		left[blankRow][blankCol] = bot[blankRow][blankCol - 1]
+
+		left[blankRow][blankCol - 1] = 0
+
+		leftNode = Node(0, depth, left)
+		List.append(leftNode)
+		nodes_expanded += 1
+
+	if blankCol != 2:
+		right = currNode.puzzle_state
+
+		right[blankRow][blankCol] = right[blankRow][blankCol + 1]
+
+		right[blankRow][blankCol + 1] = 0
+
+		rightNode = Node(0, depth, right)
+		List.append(rightNode)
+		nodes_expanded += 1
+
+
+
+	return List, nodes_expanded
 
 
 
@@ -34,34 +129,55 @@ def searchAlgorithm(problem, choice):
 	# Set up data/storage needed for algorithm
 	# i.e create node object with correct heuristics, depth and heapq
 	# IMPORTANT: depth = cost for this puzzle solution --> g(n) = depth level
+	nodes_expanded = 0
+	max_queue_nodes = 0
+
 	if choice == 1:
 		init_node = Node(0, 0, problem)
 
 	if choice == 2:
 		# Get heuristic for misplaced tile
 
-		init_node = Node(0,0, problem)
+		h = misplaced(problem)
+		init_node = Node(h,0, problem)
 
 	if choice == 3:
 		# Get heuristic for manhattan distance
-		init_node = Node(0, 0, problem)
+		h = manhattan(problem)
+		init_node = Node(h, 0, problem)
 	# Push the node to the priority queue
+	pq = []
+	heappush(pq, init_node)
+	print len(pq)
 
 	# Begin loop
+	goal = False
 
-	# If queue length = 0, exit
+	while not goal:
 
-	# Make temp node = current node in front of queue
+	# Make temp node = current node in front of queue & pop
+		temp_node = heappop(pq)
+		print temp_node.puzzle_state
 
-	# pop node from queue
+	# check if temp node = solution, if it is -> print and exit loop
+		if temp_node.puzzle_state == final_state:
+			goal = True
+			print 'Goal!'
+			print temp_node.puzzle_state
+			print 'To solve this problem the search algorithm expanded a total of ' + nodes_expanded + 'nodes.'
+			print 'The maximum number of nodes in the queue at any one time was ' + max_queue_nodes + '.'
+			print 'The depth of the goal node was ' + temp_node.depth + '.'
 
-	# check if temp node = solution
+	# else, expand node (swap up,down,left,right) || expand method returns priority queue and nodes_expanded
+	# during expansion, update nodes_expanded --> expand(temp_node, nodes_expanded)
+	# (update depth in object during expansion)
+		tempList, nodes_expanded = expand(temp_node, nodes_expanded)
 
-	# if true, exit loop
+	# push the children nodes into the queue, and use heapify to sort by priority
+	# also update the heuristics to correct one
 
-	# else, expand node (swap up,down,left,right)
+	# update max_queue_nodes
 
-	# push the children nodes into the queue, and use heapify to auto sort
 
 
 
@@ -127,15 +243,12 @@ if __name__ == '__main__':
 
 		if choice == "1":
 			searchAlgorithm(puzzle, 1)
-			print 'UCS method'
 			menu = True
 		elif choice == "2":
 			searchAlgorithm(puzzle, 2)
-			print 'misplaced tile heuristic method'
 			menu = True
 		elif choice == "3":
 			searchAlgorithm(puzzle, 3)
-			print 'manhattan distance heuristic method'
 			menu = True
 		else:
 			print 'Invalid choice. Try again.'
